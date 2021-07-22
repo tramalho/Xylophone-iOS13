e12e1260868e11b2ede9dd4fa12e162b64371242
tramalho <thiagofacul@gmail.com>
2021-07-22 20:21:29 -0300

    77. Boss Challenge

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    private var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        if let currentTitle = sender.currentTitle {
+            
+            sender.alpha = 0.5
+                        
+            print("start")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                print("end")
+                self.playSound(soundName: currentTitle)
+                sender.alpha = 1.0
+            }
+        }
+    }
+    
+    private func playSound(soundName: String) {
+        if let resource = Bundle.main.url(forResource: soundName, withExtension: "wav") {
+            player = try! AVAudioPlayer(contentsOf: resource)
+            player.play()
+        }
+    }
 }
 
